d8a92bb67042f9cee5e91080f5a468fc124a3ec2
Peter Durham <peterdur@microsoft.com>
2019-05-10 11:10:57 -0700

    Tidy up main.js

--- main.js
+++ main.js
@@ -1,16 +1,8 @@
-// Modules to control application life and create native browser window
 const {app, ipcMain, BrowserWindow} = require('electron')
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 let windows = [];
 let nextWindowIndex = 0;
-
-function onAppReady() {
-  createWindow();
-  createWindow();
-  createWindow();
-}
+let windowCount = 0;
 
 function createWindow() {
   const window = new BrowserWindow({
@@ -22,14 +14,34 @@ function createWindow() {
   })
 
   const windowIndex = nextWindowIndex++;
+  windowCount++;
   windows[windowIndex] = window;
 
   window.loadFile('window.html')
 
   window.on('closed', function () {
     windows[windowIndex] = null;
+    windowCount--;
   })
-}
+};
+
+app.on('ready', () => {
+  createWindow();
+  createWindow();
+  createWindow();
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+});
+
+app.on('activate', function () {
+  if (windowCount === 0) {
+    createWindow()
+  }
+});
 
 ipcMain.on('request-renderer-index', (event, arg) => {
   console.log('received request-renderer-index', arg);
@@ -38,18 +50,9 @@ ipcMain.on('request-renderer-index', (event, arg) => {
       windows[i].webContents.send('set-renderer-index', i);
     }
   }
-})
+});
 
 ipcMain.on('message-a', (event, arg) => {
   console.log('received message-a', arg);
-})
+});
 
-app.on('ready', onAppReady)
-
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
-
-app.on('activate', function () {
-  if (renderer === null) createWindow()
-})
