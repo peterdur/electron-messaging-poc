bd30c8530e935ea012c82f766081f802ebf3d8fa
Peter Durham <peterdur@microsoft.com>
2019-05-15 16:20:22 -0700

    Tweak quotes

--- window-preload.js
+++ window-preload.js
@@ -2,22 +2,22 @@ const { ipcRenderer } = require("electron");
 const { log } = require('./poc');
 
 window.poc = {
-  getFromPreload: () => "got this string from preload!",
+  getFromPreload: () => 'got this string from preload!',
 
   getCounter: () => {
-    ipcRenderer.send("poc/get-counter");
+    ipcRenderer.send('poc/get-counter');
   },
 
   increment: () => {
-    ipcRenderer.send("poc/increment");
+    ipcRenderer.send('poc/increment');
   },
 
   newWindow: () => {
-    ipcRenderer.send("poc/new-window");
+    ipcRenderer.send('poc/new-window');
   },
 
   onUpdateCounter: callback => {
-    ipcRenderer.on("poc/update-counter", callback);
+    ipcRenderer.on('poc/update-counter', callback);
   },
 
   log: (s) => {
