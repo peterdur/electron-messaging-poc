b408ec5f3c24f8a2bf43ebaa16ebfa2f99437110
Peter Durham <peterdur@microsoft.com>
2019-05-15 15:33:01 -0700

    Using pocOnMessage in preload

--- window-preload.js
+++ window-preload.js
@@ -1,1 +1,8 @@
+const { ipcRenderer } = require('electron');
+const { log } = require('./poc');
+
 window.pocGetFromPreload = () => 'got this string from preload!';
+
+window.pocOnMessage = (message, callback) => {
+    ipcRenderer.on(message, callback);
+}
