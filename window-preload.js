44eeae15e9e5ae076374db5d862126bf5c62977e
Peter Durham <peterdur@microsoft.com>
2019-05-16 09:39:53 -0700

    pocSend used widely

--- window-preload.js
+++ window-preload.js
@@ -1,37 +1,32 @@
-const { ipcRenderer } = require('electron');
-const { log } = require('./poc');
+const { ipcRenderer } = require("electron");
+const { log } = require("./poc");
 
 window.poc = {
-  getFromPreload: () => 'got this string from preload!',
-
-  getCounter: () => {
-    ipcRenderer.send('poc/get-counter');
-  },
-
-  increment: () => {
-    ipcRenderer.send('poc/increment');
-  },
-
-  newWindow: () => {
-    ipcRenderer.send('poc/new-window');
-  },
+  getFromPreload: () => "got this string from preload!",
 
   onUpdateCounter: callback => {
-    ipcRenderer.on('poc/update-counter', callback);
+    ipcRenderer.on("poc/update-counter", callback);
   },
-
-  log: (s) => {
-      log(s);
-  }
 };
 
-window.addEventListener("message", (event) => {
-    const messageType = event.data.messageType;
-    const payload = event.data.payload;
-    
-    switch(messageType) {
-        case 'getCounter':
-            ipcRenderer.send('poc/get-counter');
-            break;
-    }
+window.addEventListener("message", event => {
+  const messageType = event.data.messageType;
+  const payload = event.data.payload;
+
+  switch (messageType) {
+    case "getCounter":
+      ipcRenderer.send("poc/get-counter");
+      break;
+
+    case "increment":
+      ipcRenderer.send("poc/increment");
+      break;
+
+    case "newWindow":
+      ipcRenderer.send("poc/new-window");
+      break;
+
+    case "log":
+      log(payload);
+  }
 });
