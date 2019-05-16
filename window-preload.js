a6297412677a69b8941cc0a86a129f008d5cd537
Peter Durham <peterdur@microsoft.com>
2019-05-16 10:12:42 -0700

    Normalize to single quotes

--- window-preload.js
+++ window-preload.js
@@ -1,36 +1,36 @@
-const { ipcRenderer } = require("electron");
-const { log } = require("./poc");
+const { ipcRenderer } = require('electron');
+const { log } = require('./poc');
 
 window.poc = {
-  getFromPreload: () => "got this string from preload!",
+  getFromPreload: () => 'got this string from preload!',
 };
 
 const pocSend = (messageType, payload) => {
-  window.postMessage({ messageType, payload }, "*");
+  window.postMessage({ messageType, payload }, '*');
 };
 
-window.addEventListener("message", event => {
+window.addEventListener('message', event => {
   const messageType = event.data.messageType;
   const payload = event.data.payload;
 
   switch (messageType) {
-    case "getCounter":
-      ipcRenderer.send("poc/get-counter");
+    case 'getCounter':
+      ipcRenderer.send('poc/get-counter');
       break;
 
-    case "increment":
-      ipcRenderer.send("poc/increment");
+    case 'increment':
+      ipcRenderer.send('poc/increment');
       break;
 
-    case "newWindow":
-      ipcRenderer.send("poc/new-window");
+    case 'newWindow':
+      ipcRenderer.send('poc/new-window');
       break;
 
-    case "log":
+    case 'log':
       log(payload);
   }
 });
 
-ipcRenderer.on("poc/update-counter", (event, arg) => {
-    pocSend("updateCounter", arg);
+ipcRenderer.on('poc/update-counter', (event, arg) => {
+    pocSend('updateCounter', arg);
 });
